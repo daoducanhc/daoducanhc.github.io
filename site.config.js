17875164b1c4fbd66af651f91f7fcb1a1323fb99
ducanhc <daoducanhc@gmail.com>
2022-08-18 11:16:38 +0700

    clone from nanogen (npm run build:prod)

--- site.config.js
+++ site.config.js
@@ -1,9 +1,13 @@
+const projects = require('./src/data/projects');
+
 module.exports = {
-  build: {
-    srcPath: './src',
-    outputPath: './public'
-  },
   site: {
-    title: 'Nanogen site'
+    title: 'NanoGen',
+    description: 'Micro Static Site Generator in Node.js',
+    basePath: process.env.NODE_ENV === 'production' ? '/nanogen' : '',
+    projects
+  },
+  build: {
+    outputPath: process.env.NODE_ENV === 'production' ? './docs' : './public'
   }
 };
